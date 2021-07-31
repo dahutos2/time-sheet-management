ebf2ca7a31dfe5e6df9667223aa90ced11ddeda0
dahutos2 <dahutos2@gmail.com>
2021-07-31 21:30:04 +0900

    admin_blog_login

--- pj_blog/urls.py
+++ pj_blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 from django.views.generic import TemplateView
 
@@ -14,13 +14,13 @@ admin.site.index_title = 'メニュー'
 admin.site.unregister(Group)
 admin.site.disable_action('delete_selected')
 # 実はページを表示するだけならこのように1行で書くことが出来ます。
-#index_view = TemplateView.as_view(template_name="registration/index.html")
+index_view = TemplateView.as_view(template_name="registration/index.html")
 
 urlpatterns = [
     path('dahutos-admin/', admin.site.urls),
-    path('', include("blog.urls")),
-    #path("", login_required(index_view), name="index"),
-    #path('', include("django.contrib.auth.urls")),
+    path("", login_required(index_view), name="index"),
+    path('blog/', include("blog.urls")),
+    path('', include("django.contrib.auth.urls")),
     #path("signup/", views.SignUpView.as_view(), name="signup"),
     #path('activate/<uidb64>/<token>/', views.ActivateView.as_view(), name='activate'),
 ]
