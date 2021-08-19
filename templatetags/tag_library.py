28f02cdb158d11eb5f66a2a1838ccb108dd6dfcf
Daichi Okumura <87911183+dahutos2@users.noreply.github.com>
2021-08-19 23:22:45 +0900

    Create tag_library.py

---
+++ templatetags/tag_library.py
@@ -0,0 +1,10 @@
+from django import template
+import datetime
+import jpholiday
+register = template.Library()
+ 
+@register.filter(name="jpholiday")
+def jpholiday_judge(day):
+   val = jpholiday.is_holiday(day)
+   
+   return val
