47e21795f45be9650bc26ad5e54349449c6bd7a0
Daichi Okumura <87911183+dahutos2@users.noreply.github.com>
2021-08-21 08:57:11 +0900

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -91,8 +91,7 @@ class BlogAdminSite(AdminSite):
     login_form = AuthenticationForm
 
     def has_permission(self, request):
-        return request.user.is_active
-
+        return request.user.is_superuser
 
 mypage_site = BlogAdminSite(name="mypage")
 mypage_site.register(models.Post)
