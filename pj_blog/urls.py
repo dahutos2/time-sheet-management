5a8f319494805a63038a11e6ef34a0e5a4f4e7ba
Daichi Okumura <87911183+dahutos2@users.noreply.github.com>
2021-08-21 22:00:01 +0900

    Update urls.py

--- pj_blog/urls.py
+++ pj_blog/urls.py
@@ -1,21 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.decorators import login_required
-
-from blog.admin import mypage_site
-
 from blog import views
-
 from blog.models import Post
-
 from django.contrib.auth.models import Group
 
 admin.site.site_title = '管理'
 admin.site.site_header = 'システム管理サイト'
 admin.site.index_title = 'メニュー'
 admin.site.unregister(Group)
-#admin.site.disable_action('delete_selected')
-# 実はページを表示するだけならこのように1行で書くことが出来ます。
 
 urlpatterns = [
     path('dahutos-admin/', admin.site.urls),
@@ -30,5 +23,5 @@ urlpatterns = [
     path('detail/<pk>', views.Detail.as_view(), name="detail"),
     path('update/<pk>', views.Update.as_view(), name="update"),
     path('delete/<pk>', views.Delete.as_view(), name="delete"),
-    path('mypage/', mypage_site.urls),
+    path('mypage/',views.Mypage.as_view(), name="mypage"),
 ]
