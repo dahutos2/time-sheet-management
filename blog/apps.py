cb90a8fa07bb385383da8a467289ede6fe168805
Daichi Okumura <87911183+dahutos2@users.noreply.github.com>
2021-08-21 15:52:22 +0900

    Update apps.py

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class BlogConfig(AppConfig):
     name = 'blog'
-    verbose_name = 'シフト管理'
+    verbose_name = verbose_name_plural = _('システム')
