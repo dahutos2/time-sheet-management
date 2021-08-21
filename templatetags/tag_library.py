1582382906ac011847281aaf54fda6487047d773
Daichi Okumura <87911183+dahutos2@users.noreply.github.com>
2021-08-21 23:49:33 +0900

    Update tag_library.py

--- templatetags/tag_library.py
+++ templatetags/tag_library.py
@@ -8,3 +8,10 @@ def jpholiday_judge(day):
    val = jpholiday.is_holiday(day)
    
    return val
+
+@register.filter(name="editable")
+def editable_count(object,arg):
+    int = object.filter(published=arg).count()
+
+    return int
+  
