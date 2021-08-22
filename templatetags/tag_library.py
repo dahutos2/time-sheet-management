7d3cc004fd4e95e24063dcd6547796515b94cfe5
Daichi Okumura <87911183+dahutos2@users.noreply.github.com>
2021-08-22 09:17:46 +0900

    Update tag_library.py

--- templatetags/tag_library.py
+++ templatetags/tag_library.py
@@ -14,3 +14,12 @@ def editable_count(object,arg):
     int = object.filter(published=arg).count()
 
     return int
+
+@register.filter(name="three_months")
+def three_months_ago(object):
+    startdate = datetime.date(datetime.date.today().year,
+                datetime.date.today().month- 3, 1)
+    enddate = datetime.date.today()
+    post_qs = object.filter(date__range=[startdate, enddate])
+
+    return post_qs
